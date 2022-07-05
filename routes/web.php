d09a616c4cfbee74a19182fb283f3f52b76a77a1
SandeshDhakal0 <dhakalsandesh0@gmail.com>
2022-07-05 15:59:07 +0545

    first

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about-us', function(){
+    
+     ddd("test about us");
+    // echo "Test about us";
+});
